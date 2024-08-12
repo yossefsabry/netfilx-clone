cf5b7ffc12bb04d92074735b8ff3e1399a6afb71
yossefsabry <yossefsabry66@gmail.com>
2024-08-12 23:57:00 +0000

    update loading state

--- src/App.jsx
+++ src/App.jsx
@@ -8,43 +8,44 @@ import Footer from "./components/Footer";
 import Loader from "./components/Loader";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    // Simulate an asynchronous task (e.g., fetching data)
-    const fetchData = async () => {
-      try {
-        // Perform your asynchronous tasks here
-        // For example, fetching data from an API
-        const response = await fetch('https://api.example.com/data');
-        const result = await response.json();
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        // Simulate an asynchronous task (e.g., fetching data)
+        const fetchData = async () => {
+            try {
+                // Perform your asynchronous tasks here
+                // For example, fetching data from an API
+                const response = await fetch('https://api.example.com/data');
+                const result = await response.json();
 
-        // Set the data and mark loading as complete
-        setData(result);
+                // Set the data and mark loading as complete
+                setData(result);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching data:');
+                setLoading(false);
+            }
+        };
+
+        fetchData(); // Call the function to initiate the loading process
         setLoading(false);
-      } catch (error) {
-        console.error('Error fetching data:');
-        setLoading(false);
-      }
-    };
+    }, []);
 
-    fetchData(); // Call the function to initiate the loading process
-  }, []);
-
-  return (
-    <>
-      <Header />
-      {loading ? (<Loader />) : (
+    return (
         <>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path=":userId" element={<Product />} />
-          </Routes>
+            <Header />
+            {loading ? (<Loader />) : (
+                <>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path=":userId" element={<Product />} />
+                    </Routes>
+                </>
+            )}
+            <Footer />
         </>
-      )}
-      <Footer />
-    </>
-  );
+    );
 }
 
 export default App;
